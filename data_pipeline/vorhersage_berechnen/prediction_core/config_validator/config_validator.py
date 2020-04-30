3521caac2da88add45ae42d183881dda0ad9d57a
Julian <juliang98@web.de>
2020-04-30 14:57:15 +0200

    Add boilerplate code for config validator and model persistor

---
+++ data_pipeline/vorhersage_berechnen/prediction_core/config_validator/config_validator.py
@@ -0,0 +1,43 @@
+def validate_config(config):
+    '''
+    Name in documentation: 'config_validieren'
+    :param config:
+    :return:
+    '''
+    return True
+
+
+def check_completeness(config):
+    '''
+    Name in documentation: 'vollstaendigkeit_pruefen'
+    :param config:
+    :return:
+    '''
+    return True
+
+
+def check_redundancies(config):
+    '''
+    Name in documentation: 'redundanzen_pruefen'
+    :param config:
+    :return:
+    '''
+    return True
+
+
+def check_prediction_chain(config):
+    '''
+    Name in documentation: 'aufbau_kette_pruefen'
+    :param config:
+    :return:
+    '''
+    return True
+
+
+def check_training_percentage(config):
+    '''
+    Name in documentation: 'trainings_anteil_pruefen'
+    :param config:
+    :return:
+    '''
+    return True
