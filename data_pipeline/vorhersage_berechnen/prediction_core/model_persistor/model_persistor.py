6dc294d09cf970513e9bbaa1b7dbe712305e8098
Julian <juliang98@web.de>
2020-04-30 14:57:15 +0200

    Add boilerplate code for config validator and model persistor

---
+++ data_pipeline/vorhersage_berechnen/prediction_core/model_persistor/model_persistor.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import pickle
+
+
+def load():
+    '''
+    Name in documentation: 'laden'
+    :return
+    '''
+    return
+
+
+def save(all_prediction_models):
+    '''
+    Name in documentation: 'speichern'
+    :param all_prediction_models:
+    :return:
+    '''
+    return
