7b9e0f32c2f526ba06392babaf4cd9b317e1bf9b
Magnus Verclas <1727129@stud.hs-mannheim.de>
2020-04-30 15:05:42 +0200

    intervall_löschen angefangen und config ausgelesen

--- data_pipeline/daten_filtern/tag_drop_engine.py
+++ data_pipeline/daten_filtern/tag_drop_engine.py
@@ -1,6 +1,61 @@
 import pandas
-import numpy
+import numpy as np
 
 def intervall_loeschen(config, klassifizierte_daten):
 
-    return zyklenfreie_daten
+    if config["room"]["WarmwWasserZyklus"]["delete"] == "True":
+        klassifizierte_daten.loc[klassifizierte_daten.WarmWasserTag == True , 'roomTemp'] = np.nan
+    if config["room"]["OfenZyklus"]["delete"] == "True":
+        klassifizierte_daten.loc[klassifizierte_daten.OfenTag == True , 'roomTemp'] = np.nan
+    if config["room"]["LüfterZyklus"]["delete"] == "True":
+        klassifizierte_daten.loc[klassifizierte_daten.LuefterTag == True , 'roomTemp'] = np.nan
+    if config["room"]["AbtauZyklus"]["delete"] == "True":
+        klassifizierte_daten.loc[klassifizierte_daten.AbtauTag == True , 'roomTemp'] = np.nan
+
+    if config["condenser"]["WarmwWasserZyklus"]["delete"] == "True":
+        klassifizierte_daten.loc[klassifizierte_daten.WarmWasserTag == True , 'condenserTemp'] = np.nan
+    if config["condenser"]["OfenZyklus"]["delete"] == "True":
+        klassifizierte_daten.loc[klassifizierte_daten.OfenTag == True , 'condenserTemp'] = np.nan
+    if config["condenser"]["LüfterZyklus"]["delete"] == "True":
+        klassifizierte_daten.loc[klassifizierte_daten.LuefterTag == True , 'condenserTemp'] = np.nan
+    if config["condenser"]["AbtauZyklus"]["delete"] == "True":
+        klassifizierte_daten.loc[klassifizierte_daten.AbtauTag == True , 'condenserTemp'] = np.nan
+
+    if config["evaporator"]["WarmwWasserZyklus"]["delete"] == "True":
+        klassifizierte_daten.loc[klassifizierte_daten.WarmWasserTag == True , 'evaporatorTemp'] = np.nan
+    if config["evaporator"]["OfenZyklus"]["delete"] == "True":
+        klassifizierte_daten.loc[klassifizierte_daten.OfenTag == True , 'evaporatorTemp'] = np.nan
+    if config["evaporator"]["LüfterZyklus"]["delete"] == "True":
+        klassifizierte_daten.loc[klassifizierte_daten.LuefterTag == True , 'evaporatorTemp'] = np.nan
+    if config["evaporator"]["AbtauZyklus"]["delete"] == "True":
+        klassifizierte_daten.loc[klassifizierte_daten.AbtauTag == True , 'evaporatorTemp'] = np.nan
+
+    if config["inlet"]["WarmwWasserZyklus"]["delete"] == "True":
+        klassifizierte_daten.loc[klassifizierte_daten.WarmWasserTag == True , 'inletTemp'] = np.nan
+    if config["inlet"]["OfenZyklus"]["delete"] == "True":
+        klassifizierte_daten.loc[klassifizierte_daten.OfenTag == True , 'inletTemp'] = np.nan
+    if config["inlet"]["LüfterZyklus"]["delete"] == "True":
+        klassifizierte_daten.loc[klassifizierte_daten.LuefterTag == True , 'inletTemp'] = np.nan
+    if config["inlet"]["AbtauZyklus"]["delete"] == "True":
+        klassifizierte_daten.loc[klassifizierte_daten.AbtauTag == True , 'inletTemp'] = np.nan
+
+    if config["outlet"]["WarmwWasserZyklus"]["delete"] == "True":
+        klassifizierte_daten.loc[klassifizierte_daten.WarmWasserTag == True , 'outletTemp'] = np.nan
+    if config["outlet"]["OfenZyklus"]["delete"] == "True":
+        klassifizierte_daten.loc[klassifizierte_daten.OfenTag == True , 'outletTemp'] = np.nan
+    if config["outlet"]["LüfterZyklus"]["delete"] == "True":
+        klassifizierte_daten.loc[klassifizierte_daten.LuefterTag == True , 'outletTemp'] = np.nan
+    if config["outlet"]["AbtauZyklus"]["delete"] == "True":
+        klassifizierte_daten.loc[klassifizierte_daten.AbtauTag == True , 'outletTemp'] = np.nan
+
+    if config["freshAirIntake"]["WarmwWasserZyklus"]["delete"] == "True":
+        klassifizierte_daten.loc[klassifizierte_daten.WarmWasserTag == True , 'freshAirIntakeTemp'] = np.nan
+    if config["freshAirIntake"]["OfenZyklus"]["delete"] == "True":
+        klassifizierte_daten.loc[klassifizierte_daten.OfenTag == True , 'freshAirIntakeTemp'] = np.nan
+    if config["freshAirIntake"]["LüfterZyklus"]["delete"] == "True":
+        klassifizierte_daten.loc[klassifizierte_daten.LuefterTag == True , 'freshAirIntakeTemp'] = np.nan
+    if config["freshAirIntake"]["AbtauZyklus"]["delete"] == "True":
+        klassifizierte_daten.loc[klassifizierte_daten.AbtauTag == True , 'freshAirIntakeTemp'] = np.nan
+
+
+    return klassifizierte_daten
