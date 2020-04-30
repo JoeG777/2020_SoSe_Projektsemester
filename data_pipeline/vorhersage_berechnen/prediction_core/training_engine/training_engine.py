4fe888aa6f4da32c22bc4f9e22c0fb1054a9a42a
Marius1822640 <mbrill963@gmail.com>
2020-04-30 13:53:30 +0200

    Added function framework for training_engine

---
+++ data_pipeline/vorhersage_berechnen/prediction_core/training_engine/training_engine.py
@@ -0,0 +1,21 @@
+
+
+def train(config):
+    all_models = []
+    for prediction_unit in config:
+        all_models.append(train_model(prediction_unit))
+        get_data()
+
+def save_prediction_model(all_models, config):
+    return ""
+
+
+def train_model(prediction_unit):
+    return ""
+
+
+def get_data():
+
+
+def calculate_average_score():
+    return""
