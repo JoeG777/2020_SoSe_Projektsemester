f2ff49af0eaec3a5edb30c26d7f6303ad0de3293
Magnus Verclas <1727129@stud.hs-mannheim.de>
2020-04-30 14:33:55 +0200

    Erstes einfügen der Methoden. noch ohne Funktion

--- data_pipeline/daten_filtern/filtern_api.py
+++ data_pipeline/daten_filtern/filtern_api.py
@@ -1,1 +1,13 @@
-#test
+from flask import *
+import filtern_config
+import filtern_engine
+
+app = Flask(__name__)
+
+@app.route('/filtern' , methods = ['GET' , 'POST'])
+def filtern():
+    config = filtern_config
+
+    filtern_engine.filtern(config)
+
+    return render_template('index.html')
---
+++ data_pipeline/daten_filtern/tag_drop_engine.py
@@ -0,0 +1,6 @@
+import pandas
+import numpy
+
+def intervall_loeschen(config, klassifizierte_daten):
+
+    return zyklenfreie_daten
