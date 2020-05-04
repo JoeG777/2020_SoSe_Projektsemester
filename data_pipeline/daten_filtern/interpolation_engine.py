4feb2e9793ee5acab54c3123e72f913b8a917737
Magnus Verclas <1727129@stud.hs-mannheim.de>
2020-05-04 15:50:52 +0200

    Filtern Engine angepasst dass erst ein Zyklus auf "nan" gesetzt wird dann direkt interpoliert wird, dann der nächste

--- data_pipeline/daten_filtern/interpolation_engine.py
+++ data_pipeline/daten_filtern/interpolation_engine.py
@@ -1,14 +1,13 @@
 import pandas
 
-def interpolieren(config, zyklenfreie_daten):
+def interpolieren(config, kurve,zyklus,  zyklenfreie_daten):
+    '''
+    Name in documentation: 'interpolieren'
+    :param config:
+    :param zyklenfreie_daten:
+    :return: filterd data
+    '''
 
-    # Abklären wie nur pro Zyklus Interpoliert werden kann. Ansonsten pro Kurve und dann nochmal Config ändern!
-
-    zyklenfreie_daten['room'].interpolate(method = config["room"]["WarmWasserZyklus"]["Interpolation"] , inplace = True )
-    zyklenfreie_daten['condenser'].interpolate(method = config["condenser"]["WarmWasserZyklus"]["Interpolation"] , inplace = True )
-    zyklenfreie_daten['evaporator'].interpolate(method = config["evaporator"]["WarmWasserZyklus"]["Interpolation"] , inplace = True )
-    zyklenfreie_daten['inlet'].interpolate(method = config["inlet"]["WarmWasserZyklus"]["Interpolation"] , inplace = True )
-    zyklenfreie_daten['outlet'].interpolate(method = config["outlet"]["WarmWasserZyklus"]["Interpolation"] , inplace = True )
-    zyklenfreie_daten['freshAirIntake'].interpolate(method = config["freshAirIntake"]["WarmWasserZyklus"]["Interpolation"] , inplace = True )
+    zyklenfreie_daten[kurve].interpolate(method = config[kurve][zyklus]["Interpolation"] , inplace = True)
 
     return zyklenfreie_daten
