3e2fea6611ed01d627ed606d0d5b26cfd25a1185
Magnus Verclas <1727129@stud.hs-mannheim.de>
2020-05-04 15:50:52 +0200

    Filtern Engine angepasst dass erst ein Zyklus auf "nan" gesetzt wird dann direkt interpoliert wird, dann der nächste

--- data_pipeline/daten_filtern/filtern_api.py
+++ data_pipeline/daten_filtern/filtern_api.py
@@ -4,10 +4,31 @@ import filtern_engine
 
 app = Flask(__name__)
 
-@app.route('/filtern' , methods = ['GET' , 'POST'])
+@app.route('/')
+def default():
+    '''
+
+    '''
+    return '<h1>Endpoints</h1><p>/filtern - filter data</p><p>/log - Get the logs</p>'
+
+
+
+
+@app.route('/filtern')
 def filtern():
+    '''
+    Name in documentation: 'filtern'
+    '''
     config = filtern_config["filter_options"][filtern_config["selected_value"]]
 
-    filtern_engine.filtern(config)
+    data_pipeline.daten_filtern.filtern_engine.filtern(config)
 
     return render_template('index.html')
+
+
+@app.route('/log')
+def get_logs():
+    '''
+    Name in documentation: 'get_logs'
+    '''
+    return 'logs'
--- data_pipeline/daten_filtern/tag_drop_engine.py
+++ data_pipeline/daten_filtern/tag_drop_engine.py
@@ -1,60 +1,23 @@
 import pandas
 import numpy as np
 
-def intervall_loeschen(config, klassifizierte_daten):
 
-    if config["room"]["WarmWasserZyklus"]["delete"] == "True":
-        klassifizierte_daten.loc[klassifizierte_daten.WarmWasserTag == True , 'roomTemp'] = np.nan
-    if config["room"]["OfenZyklus"]["delete"] == "True":
-        klassifizierte_daten.loc[klassifizierte_daten.OfenTag == True , 'roomTemp'] = np.nan
-    if config["room"]["LüfterZyklus"]["delete"] == "True":
-        klassifizierte_daten.loc[klassifizierte_daten.LuefterTag == True , 'roomTemp'] = np.nan
-    if config["room"]["AbtauZyklus"]["delete"] == "True":
-        klassifizierte_daten.loc[klassifizierte_daten.AbtauTag == True , 'roomTemp'] = np.nan
+"""
+Auf Basis der in der Config Datei bestimmten Zyklen und Kurven und der mitgelieferten Daten, 
+werden die entsprechenden Zyklenintervalle aus den Daten herausgelöscht, indem die Werte auf "Nan"
+gesetzt werden. Die Parameter sind die klassifizierten Daten und die Config Datei. Als return Wert kommen 
+die zyklenfreie Daten. 
+"""
 
-    if config["condenser"]["WarmWasserZyklus"]["delete"] == "True":
-        klassifizierte_daten.loc[klassifizierte_daten.WarmWasserTag == True , 'condenserTemp'] = np.nan
-    if config["condenser"]["OfenZyklus"]["delete"] == "True":
-        klassifizierte_daten.loc[klassifizierte_daten.OfenTag == True , 'condenserTemp'] = np.nan
-    if config["condenser"]["LüfterZyklus"]["delete"] == "True":
-        klassifizierte_daten.loc[klassifizierte_daten.LuefterTag == True , 'condenserTemp'] = np.nan
-    if config["condenser"]["AbtauZyklus"]["delete"] == "True":
-        klassifizierte_daten.loc[klassifizierte_daten.AbtauTag == True , 'condenserTemp'] = np.nan
+def intervall_loeschen(config, kurve , zyklus, klassifizierte_daten):
+    '''
+    Name in documentation: 'intervall_loeschen'
+    :param config:
+    :param klassifizierte_daten:
+    :return: cycle free data
+    '''
 
-    if config["evaporator"]["WarmWasserZyklus"]["delete"] == "True":
-        klassifizierte_daten.loc[klassifizierte_daten.WarmWasserTag == True , 'evaporatorTemp'] = np.nan
-    if config["evaporator"]["OfenZyklus"]["delete"] == "True":
-        klassifizierte_daten.loc[klassifizierte_daten.OfenTag == True , 'evaporatorTemp'] = np.nan
-    if config["evaporator"]["LüfterZyklus"]["delete"] == "True":
-        klassifizierte_daten.loc[klassifizierte_daten.LuefterTag == True , 'evaporatorTemp'] = np.nan
-    if config["evaporator"]["AbtauZyklus"]["delete"] == "True":
-        klassifizierte_daten.loc[klassifizierte_daten.AbtauTag == True , 'evaporatorTemp'] = np.nan
-
-    if config["inlet"]["WarmWasserZyklus"]["delete"] == "True":
-        klassifizierte_daten.loc[klassifizierte_daten.WarmWasserTag == True , 'inletTemp'] = np.nan
-    if config["inlet"]["OfenZyklus"]["delete"] == "True":
-        klassifizierte_daten.loc[klassifizierte_daten.OfenTag == True , 'inletTemp'] = np.nan
-    if config["inlet"]["LüfterZyklus"]["delete"] == "True":
-        klassifizierte_daten.loc[klassifizierte_daten.LuefterTag == True , 'inletTemp'] = np.nan
-    if config["inlet"]["AbtauZyklus"]["delete"] == "True":
-        klassifizierte_daten.loc[klassifizierte_daten.AbtauTag == True , 'inletTemp'] = np.nan
-
-    if config["outlet"]["WarmWasserZyklus"]["delete"] == "True":
-        klassifizierte_daten.loc[klassifizierte_daten.WarmWasserTag == True , 'outletTemp'] = np.nan
-    if config["outlet"]["OfenZyklus"]["delete"] == "True":
-        klassifizierte_daten.loc[klassifizierte_daten.OfenTag == True , 'outletTemp'] = np.nan
-    if config["outlet"]["LüfterZyklus"]["delete"] == "True":
-        klassifizierte_daten.loc[klassifizierte_daten.LuefterTag == True , 'outletTemp'] = np.nan
-    if config["outlet"]["AbtauZyklus"]["delete"] == "True":
-        klassifizierte_daten.loc[klassifizierte_daten.AbtauTag == True , 'outletTemp'] = np.nan
-
-    if config["freshAirIntake"]["WarmWasserZyklus"]["delete"] == "True":
-        klassifizierte_daten.loc[klassifizierte_daten.WarmWasserTag == True , 'freshAirIntakeTemp'] = np.nan
-    if config["freshAirIntake"]["OfenZyklus"]["delete"] == "True":
-        klassifizierte_daten.loc[klassifizierte_daten.OfenTag == True , 'freshAirIntakeTemp'] = np.nan
-    if config["freshAirIntake"]["LüfterZyklus"]["delete"] == "True":
-        klassifizierte_daten.loc[klassifizierte_daten.LuefterTag == True , 'freshAirIntakeTemp'] = np.nan
-    if config["freshAirIntake"]["AbtauZyklus"]["delete"] == "True":
-        klassifizierte_daten.loc[klassifizierte_daten.AbtauTag == True , 'freshAirIntakeTemp'] = np.nan
+    if config[kurve][zyklus]["delete"] == "True":
+        klassifizierte_daten.loc[klassifizierte_daten.zyklus == True , kurve] = np.nan
 
     return klassifizierte_daten
