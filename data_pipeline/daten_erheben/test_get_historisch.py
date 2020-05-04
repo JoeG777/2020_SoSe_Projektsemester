caffc792499c3861636f253862a6ea3a181dc129
ArbnorZ <45542677+ArbnorZ@users.noreply.github.com>
2020-05-04 14:04:23 +0200

    First implementation of an unit test

---
+++ data_pipeline/daten_erheben/test_get_historisch.py
@@ -0,0 +1,11 @@
+import unittest
+import data_pipeline.daten_erheben.get_historisch as his
+
+
+class MyTestCase(unittest.TestCase):
+    def test_something(self):
+        self.assertEqual(his.get_timestamp_dwd("202001050000"), "2020-01-05T00:00:00Z")
+
+
+if __name__ == '__main__':
+    unittest.main()
