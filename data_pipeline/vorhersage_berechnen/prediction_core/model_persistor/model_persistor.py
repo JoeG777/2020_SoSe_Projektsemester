035f7c34be934b2320d17058433c6ea6a410c118
Julian <juliang98@web.de>
2020-05-04 15:11:10 +0200

    Add model persistor

--- data_pipeline/vorhersage_berechnen/prediction_core/model_persistor/model_persistor.py
+++ data_pipeline/vorhersage_berechnen/prediction_core/model_persistor/model_persistor.py
@@ -1,13 +1,22 @@
-import pandas as pd
 import pickle
+from data_pipeline.vorhersage_berechnen.prediction_core.exception.prediction_core_exceptions import PersistorException
 
+file_name = "current_model.p"
 
 def load():
     '''
     Name in documentation: 'laden'
     :return
     '''
-    return
+    current_model = None
+
+    try:
+        current_model = pickle.load(open(file_name, "rb"))
+    except Exception as ex:
+        # should log the actual exception here --> repr(ex)
+        raise PersistorException('Error while loading current_model: ' + ex.__class__.__name__)
+
+    return current_model
 
 
 def save(all_prediction_models):
@@ -16,4 +25,8 @@ def save(all_prediction_models):
     :param all_prediction_models:
     :return:
     '''
-    return
+    try:
+        pickle.dump(all_prediction_models, open(file_name, "wb"))
+    except Exception as ex:
+        # should log the actual exception here --> repr(ex)
+        raise PersistorException('Error while save current_model.p: ' + ex.__class__.__name__)
