8316066cc62176b8e828a2660c98009b3ff2e0dd
Matteo <1824001@stud.hs-mannheim.de>
2020-05-04 11:43:20 +0200

    Documentation added to the 'datenerhebung_api'.

--- data_pipeline/daten_erheben/datenerhebung_api.py
+++ data_pipeline/daten_erheben/datenerhebung_api.py
@@ -5,15 +5,29 @@ import data_pipeline.daten_erheben.config as con
 
 app = Flask(__name__)
 
+"""
+Leitet beim ersten Aufruf des Servers auf die URL /index weiter.
+"""
+
 @app.route('/')
 def start():
 
     return redirect(url_for('index'))
 
+"""
+Öffnet die index.html zur Bedienung der Applikation.
+"""
+
 @app.route('/index')
 def index():
 
     return render_template('index.html')
+
+"""
+Wird aufgerufen, sobald die historischen Wetterdaten aktualisiert werden sollen. 
+Dabei wird durch einen request aus den Parametern die URL zum Download der ZIP-Datei entnommen.
+Anschließend wird die Methode: 'historische_daten_erheben(url)' aufgerufen und erneut die index.html geöffnet.
+"""
 
 @app.route('/historischeDatenerhebung', methods = ['POST'])
 def historische_datenerhebung():
@@ -23,6 +37,12 @@ def historische_datenerhebung():
 
     his.historische_daten_erheben(urlHistorisch)
     return render_template('index.html')
+
+"""
+Wird aufgerufen, sobald die Vorhersage-Wetterdaten aktualisiert werden sollen.
+Dabei wird durch einen request aus den Parametern die URL zum Download der KMZ-Datei entnommen.
+Anschließend wird die Methode: 'vorhersage_daten_erheben(url)' aufgerufen und erneut die index.html geöffnet.
+"""
 
 @app.route('/forecastDatenerhebung', methods = ['POST'])
 def forecast_datenerhebung():
