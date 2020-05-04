1d0155ba9cd675d4944e3e24fc9a54eb4da4517b
Magnus Verclas <1727129@stud.hs-mannheim.de>
2020-05-04 10:33:20 +0200

    Config angepasst und interpolation_engine.py anfangen

--- data_pipeline/daten_filtern/interpolation_engine.py
+++ data_pipeline/daten_filtern/interpolation_engine.py
@@ -1,5 +1,5 @@
 import pandas
 
 def interpolieren(config, zyklenfreie_daten):
-    zyklenfreie_daten['room'].interpolate(method = config["room"]["Interpolation"])
-    return gefilterte_daten
+    zyklenfreie_daten['room'].interpolate(method = config["room"]["Interpolation"] , inplace = True )
+    return zyklenfreie_daten
