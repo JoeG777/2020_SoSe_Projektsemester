e6bf49c58f3d9bfa9b6a5a3ba3ecd43652a203f0
ValeriaAzzolina <valeriaazzolina@outlook.de>
2020-05-04 16:21:25 +0200

    trainingsdata editing engine angefangen: daten_erweitern() und daten_markieren() für Abtauzyklus und Warmwasseraufbereitung. Config Datei erstellt und entsprechend Parameter eingefügt.

---
+++ data_pipeline/daten_klassifizieren/config.py
@@ -0,0 +1,27 @@
+classification_config = {
+
+    "datasource_raw_data" : (host, port, username, database, password),
+    "datasource_training_data" : (host, port, username, database, password),
+    "datasource_classified_data" : (host, port, username, database, password),
+    "timeframe" :("2020-01-05 00:00", "2020-02-05 00:00"),
+    "selected_event" :"Abtauzyklus",
+    "new_classifier_method": "kNN",
+    # TODO : in ChangeLog eintragen
+    "selected_event_options": {"Abtauzyklus","Warmwasseraufbereitung","Ofennutzung","Lüfterstufen"},
+    "event_features" : {
+        "start_marker": 1.0,
+        "start_deriv": 1.0,
+        "start_evap": 0.0,
+        "start_ch": -0.06,
+        "end_marker": -1.0,
+        "end_deriv": 0.5,
+        "end_shift": -1.0,
+        "del_marker": 0.0
+    },
+    "test_sample_size" : 0.2,
+    "classification_method" : "SVM",
+    "classification_method_options" : {
+        "SVM" : "model = SVM()",
+        "kNN" : "model = KNeighbors()"
+    }
+}
---
+++ data_pipeline/daten_klassifizieren/trainingsdata_editing_engine.py
@@ -0,0 +1,107 @@
+import pandas as pd
+#import sklearn
+#import LogWriter
+#import DBConnector
+
+
+def daten_erweitern(config):
+    df = pd.DataFrame()
+    event = config['selected_event']
+    db_raw = config['datasource_raw_data']
+    timeframe = config['timeframe']
+
+    if event == 'Abtauzyklus':
+        #evaporator = pd.Series(DB-Connector.daten_lesen(db_raw, 206, timeframe), index=Evaporator)
+        #condensor = pd.Series(DB-Connector.daten_lesen(db_raw, 205, timeframe), index=Condensor)
+        #df = pd.concat([evaporator, condensor], axis=1)
+        #durchschnittliche Steigung an diesem Punkt
+        df['Condensor_deriv'] = (df['Condensor'].shift(-1) - (df['Condensor'].shift(1))) / 2
+        df['Evaporator_deriv'] = (df['Evaporator'].shift(-1) - (df['Evaporator'].shift(1))) / 2
+
+    elif event == 'Warmwasseraufbereitung':
+        #room = pd.Series(DB-Connector.daten_lesen(db_raw, 210, timeframe), index=Room)
+        #df = pd.concat([room], axis=1)
+        #durchschnittliche Ableitung
+        df['Room_deriv'] = (df['Room'].shift(-1)-df['Room'].shift(1)) / 2
+
+
+
+def daten_markieren(config):
+    df = pd.DataFrame()
+    event = config['selected_event']
+    db_train = config['datasource_training_data']
+    timeframe = config['timeframe']
+
+    if event == 'Abtauzyklus':
+        #Parameter
+        start_marker = config['event_features']['start_marker']  #1.0
+        start_deriv = config['event_features']['start_deriv']  #1.0
+        start_evap = config['event_features']['start_evap']  #0.0
+        end_marker = config['event_features']['end_marker']  #-1.0
+        end_deriv = config['event_features']['end_deriv']  #0.5
+        end_shift = config['event_features']['end_shift']  #-1.0
+        del_marker = config['event_features']['del_marker']  #0.0
+
+        df['Abtaumarker'] = 0
+        #Markierung Anfang
+        #durchschnittliche Steigung >= 1 & durchschnittliche Steigung davor < 1 & Temperatur Verdampfer <= 0
+        df.loc[(df['Evaporator_deriv']>= start_deriv) & (df['Evaporator_deriv'].shift(1) < start_deriv) & (df['Evaporator'] <= start_evap), 'Abtaumarker'] = start_marker
+        #Markierung Ende
+        #absolute durchschnittliche Steigung <= 0.5 & durchschnittliche Steigung vor drei Messungen < -1.0
+        df.loc[(abs(df['Evaporator_deriv']) <= end_deriv) & (df['Evaporator_deriv'].shift(3) < end_shift), 'Abtaumarker'] = end_marker
+        # doppelte Endmarker raus
+        df.loc[df['Abtaumarker'].shift(1) == end_marker, 'Abtaumarker'] = del_marker
+
+        #Zyklenbereich markieren
+        df['Abtauzyklus'] = False
+        spaces = df.loc[(df['Abtaumarker'] == start_marker) | (df['Abtaumarker'] == end_marker)].index.tolist()
+        len(spaces)
+        for i in range(0, len(spaces), 2):
+            df.loc[spaces[i]:spaces[i+1], 'Abtauzyklus'] = True
+
+    elif event == 'Warmwasseraufbereitung':
+        #Parameter
+        start_marker = config['event_features']['start_marker'] #1.0
+        start_deriv = config['event_features']['start_deriv'] #-0.06
+        start_ch = config['event_features']['start_ch'] #-0.06
+        end_marker = config['event_features']['end_marker'] #-1
+        end_deriv = config['event_features']['end_deriv'] #0.06
+        end_shift = config['event_features']['end_shift'] #30
+        del_marker = config['event_features']['del_marker'] #10
+
+        df['Warmwassermarker'] = 0
+        #Markierung Anfang
+        #durchschnittliche Ableitung Room <= -0.06
+        df.loc[(df['Room_deriv'] <= start_deriv) & (df['Room_deriv'] <= start_ch), 'Warmwassermarker'] = start_marker
+        #Löschen doppelter oder dreifacher Startpunkte
+        df.loc[(df['Warmwassermarker'] == start_marker) & ((df['Warmwassermarker'].shift(-1) == start_marker) |
+                                                           (df['Warmwassermarker'].shift(-2) == start_marker)), 'Warmwassermarker'] = del_marker
+        #Markierung Ende
+        #durchschnittliche Ableitung Room <= 0.06
+        df.loc[df['Room_deriv'].shift(end_shift) >= end_deriv, 'Warmwassermarker'] = end_marker
+        #Löschen doppelter oder dreifacher Endpunkte
+        df.loc[(df['Warmwassermarker'] == end_marker) & ((df['Warmwassermarker'].shift(-1) == end_marker) |
+                                                         (df['Warmwassermarker'].shift(-2) == end_marker)), 'Warmwassermarker'] = del_marker
+        #kann nur zwischen 22-8Uhr auftreten
+        df.loc[(df['Warmwassermarker'].index.hour > 7) & (df['Warmwassermarker'].index.hour < 22), 'Warmwassermarker'] = 0
+
+        #Zyklenbereich markieren
+        df['Warmwasserzyklus'] = False
+        in_cycle = False
+        count_start = 0
+        count_end = 0
+        count_in = 0
+        for i in range(0, len(df)):
+            current = df.iat[i, 6]
+            if in_cycle and (current == 0 or current == 1):
+                count_in += 1
+                df.iat[i, 6] = 10
+                current = 10
+            if current == 1:
+                count_start += 1
+                in_cycle = True
+            df.iat[i, 7] = in_cycle
+            if current == -1:
+                count_end += 1
+                in_cycle = False
+    #DB-Connector.daten_schreiben(db_train, df)
