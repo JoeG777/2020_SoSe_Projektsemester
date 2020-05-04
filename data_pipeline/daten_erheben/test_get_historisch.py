b7ac7dd35853a9d4d81959e4c4ad319901407eb0
ArbnorZ <45542677+ArbnorZ@users.noreply.github.com>
2020-05-04 14:51:15 +0200

    Added unittests for get_historisch.py

--- data_pipeline/daten_erheben/test_get_historisch.py
+++ data_pipeline/daten_erheben/test_get_historisch.py
@@ -1,11 +1,14 @@
 import unittest
 import data_pipeline.daten_erheben.get_historisch as his
+from data_pipeline.daten_erheben.exception import url_exception
 
 
 class MyTestCase(unittest.TestCase):
     def test_get_timestamp_dwd(self):
         self.assertEqual(his.get_timestamp_dwd("202001050000"), "2020-01-05T00:00:00Z")
 
+    def test_get_temp_data(self):
+        self.assertRaises(url_exception, his.get_temp_data, '4r41.de')
 
 if __name__ == '__main__':
     unittest.main()
