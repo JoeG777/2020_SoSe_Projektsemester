cf0d49bac287fca78753ee3dc2e1ec7ae858c7ec
Marius1822640 <mbrill963@gmail.com>
2020-05-04 16:01:53 +0200

    implemented training

--- data_pipeline/vorhersage_berechnen/prediction_core/training_engine/training_engine.py
+++ data_pipeline/vorhersage_berechnen/prediction_core/training_engine/training_engine.py
@@ -1,3 +1,15 @@
+import data_pipeline.db_connector.src.read_manager.read_manager as rm
+
+default_measurement = "measurement"
+
+register_dict = {
+    "Fresh Air Intake": "201",
+    "Inlet": "202",
+    "Room": "210",
+    "Outlet": "204",
+    "Condenser": "205",
+    "Evaporator": "206"
+}
 
 
 def train(config):
@@ -5,6 +17,7 @@ def train(config):
     for prediction_unit in config:
         all_models.append(train_model(prediction_unit))
         get_data()
+
 
 def save_prediction_model(all_models, config):
     return ""
@@ -14,8 +27,20 @@ def train_model(prediction_unit):
     return ""
 
 
-def get_data():
+def get_data(register):
+    print("getting data")
+    return rm.read_register_of_measurement("temperature_register", register_dict[register])
+
+def get_all_data():
+    keys = register_dict.keys()
+    data = []
+    for key in keys:
+
+
+
 
 
 def calculate_average_score():
-    return""
+    return " "
+
+print(get_data("Fresh Air Intake"))
