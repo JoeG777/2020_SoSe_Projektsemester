38f86e9a971c6a19f84612fd89c0c6524c4e93fc
Magnus Verclas <1727129@stud.hs-mannheim.de>
2020-05-04 11:17:09 +0200

    Config angepasst und interpolation_engine.py anfangen

--- data_pipeline/daten_filtern/interpolation_engine.py
+++ data_pipeline/daten_filtern/interpolation_engine.py
@@ -1,5 +1,14 @@
 import pandas
 
 def interpolieren(config, zyklenfreie_daten):
-    zyklenfreie_daten['room'].interpolate(method = config["room"]["Interpolation"] , inplace = True )
+
+    # Abklären wie nur pro Zyklus Interpoliert werden kann. Ansonsten pro Kurve und dann nochmal Config ändern!
+
+    zyklenfreie_daten['room'].interpolate(method = config["room"]["WarmWasserZyklus"]["Interpolation"] , inplace = True )
+    zyklenfreie_daten['condenser'].interpolate(method = config["condenser"]["WarmWasserZyklus"]["Interpolation"] , inplace = True )
+    zyklenfreie_daten['evaporator'].interpolate(method = config["evaporator"]["WarmWasserZyklus"]["Interpolation"] , inplace = True )
+    zyklenfreie_daten['inlet'].interpolate(method = config["inlet"]["WarmWasserZyklus"]["Interpolation"] , inplace = True )
+    zyklenfreie_daten['outlet'].interpolate(method = config["outlet"]["WarmWasserZyklus"]["Interpolation"] , inplace = True )
+    zyklenfreie_daten['freshAirIntake'].interpolate(method = config["freshAirIntake"]["WarmWasserZyklus"]["Interpolation"] , inplace = True )
+
     return zyklenfreie_daten
