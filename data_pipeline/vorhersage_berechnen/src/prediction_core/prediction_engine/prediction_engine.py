239fb16e2e23a0fecc2c0b884304fa1cad59b445
Marius1822640 <mbrill963@gmail.com>
2020-05-05 15:03:57 +0200

    Refactored file structure + made train recieve the full config.

--- data_pipeline/vorhersage_berechnen/src/prediction_core/prediction_engine/prediction_engine.py
+++ data_pipeline/vorhersage_berechnen/src/prediction_core/prediction_engine/prediction_engine.py
@@ -1,69 +1,50 @@
+from influxdb import InfluxDBClient
+from sklearn import linear_model
 import pandas as pd
-import data_pipeline.vorhersage_berechnen.src.prediction_core.config_validator.config_validator as cfg_validator
-import data_pipeline.vorhersage_berechnen.src.prediction_core.model_persistor.model_persistor as model_persistor
-import data_pipeline.db_connector.src.read_manager.read_manager as db_conn
-
-OUTDOOR_REGISTER = '210'
-MEASUREMENT = 'temperature_register'
-DATABASE = 'nilan'
+import numpy as np
+import data_pipeline.vorhersage_berechnen.prediction_core.config_validator.config_validator as cfg_validator
+import data_pipeline.vorhersage_berechnen.prediction_core.model_persistor.model_persistor as model_persistor
 
 
 def calculate_prediction(config):
     cfg_validator.validate_config(config)
 
-    selected_value = config.get("selected_value")
-    all_prediction_units = config.get("prediction_options").get(selected_value)
+    selected_option = config.get("selected_value")
+    all_prediction_units = config.get("prediction_options").get(selected_option)
 
-    known_data_sources = db_conn.read_register_of_measurement(DATABASE, MEASUREMENT, OUTDOOR_REGISTER)
-    known_data_sources = known_data_sources.rename(columns={'valueScaled': 'outdoor'})
-    known_data_sources['time'] = pd.to_datetime(known_data_sources['time'])
-    known_data_sources = known_data_sources.set_index('time')
-    known_data_sources = known_data_sources.resample(rule='1S').bfill()
+    # TODO ADD OUTDOOR DATA RETRIEVAL HERE
+    known_data_sources = ['outdoor']
 
     all_prediction_models = model_persistor.load()
 
+    # TODO check if all_prediction_models are up to date
+
     while all_prediction_units:
-        print(known_data_sources.head())
         for prediction_unit in all_prediction_units:
             independent_data = prediction_unit.get("independent")
-            if set(independent_data).issubset(set(known_data_sources.columns.values)):
-                apply_model(prediction_unit, known_data_sources, all_prediction_models)
-                all_prediction_units.remove(prediction_unit)
 
-    #TODO write to database
+            if set(independent_data).issubset(set(known_data_sources)):
+                apply_model(prediction_unit, known_data_sources, all_prediction_models)
+                config.remove(prediction_unit)
+
+
+    # TODO classify_prediciton
 
 
 def apply_model(prediction_unit, known_data_sources, all_prediction_models):
-    print(known_data_sources.head())
     independent_keys = prediction_unit.get("independent")
     dependent_keys = prediction_unit.get("dependent")
-    model = get_model(dependent_keys, all_prediction_models)
+    model = get_model(all_prediction_models)
 
-    independent_data = known_data_sources[independent_keys]
-    print(independent_data.head())
-    predicted_data = model.predict(independent_data)
-    # TODO include control params
-
-    count = 0
-    for key in dependent_keys:
-        known_data_sources[key] = predicted_data[:, count]
-        count += 1
-        print(known_data_sources.head())
-
-    print(known_data_sources.head())
+    # TODO extract all dependent curves out of known_data_sources (DATA FRAME)
+    # TODO apply model
+    # TODO add new predictions to known_data_sources
 
 
-def get_model(dependent_keys, all_prediction_models):
-    models = all_prediction_models.get("models")
 
-    for entry in models:
-        dependent = entry.get("dependent")
-
-        if dependent == dependent_keys:
-            return entry.get("model")
-
-    return None
-
+def get_model(all_prediction_models):
+    # TODO extract model from all_prediction_models
+    return ""
 
 def classify_prediction(db_config):
     return ""
