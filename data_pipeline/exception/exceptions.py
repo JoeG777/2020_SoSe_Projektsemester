bfbcce11f1aca831b6036c773ec68683ed6cef72
FelixEngelen <1822158@stud.hs-mannheim.de>
2020-05-05 13:31:06 +0200

    DBException hinzugefügt

--- data_pipeline/exception/exceptions.py
+++ data_pipeline/exception/exceptions.py
@@ -128,3 +128,18 @@ class RawDataException(Exception):
             return 'RawDataException, {0}'.format(self.message)
         else:
             return 'RawDataException'
+
+class DBException(Exception):
+
+    def __init__(self, *args):
+
+        if args:
+            self.message = args[0]
+        else:
+            self.message = None
+
+    def __str__(self):
+        if self.message:
+            return 'DBException, {0}'.format(self.message)
+        else:
+            return 'DBException'
