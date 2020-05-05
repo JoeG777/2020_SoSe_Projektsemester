cb1dd728eb829ae7c9c33f160964ab03086dfed7
Marius1822640 <mbrill963@gmail.com>
2020-05-05 14:27:32 +0200

    Recactored training.

--- data_pipeline/vorhersage_berechnen/prediction_core/training_engine/training_engine.py
+++ data_pipeline/vorhersage_berechnen/prediction_core/training_engine/training_engine.py
@@ -19,48 +19,42 @@ register_dict = {
 
 
 def get_data(register):
-    print("fetching data...")
     return rm.read_register_of_measurement(nilan_db, "temperature_register", register_dict[register])
 
 
-def get_temperatur():
-    print("fetching temperature...")
+def get_temperature():
     return rm.read_query_in_good(temp_db, "SELECT * FROM weatherTest LIMIT 50")
 
 
 def get_all_data():
-    print("Fetching data...")
     keys = register_dict.keys()
-    remove_me = 1
-    print("Fetching temperature")
-    df = get_temperatur()
+    df = get_temperature()
     df = df.rename(columns={'temperature': "outdoor"})
-    #-------
-    #ONLY FOR TESTING!!!
+    # -------
+    # ONLY FOR TESTING!!!
     df['time'] = pd.to_datetime(df['time'])
     df = df.set_index('time')
     df = df.resample(rule='1S').bfill()
-    #-------
-    print(df)
-    print("Done fetching temperature!")
+    # -------
     for key in keys:
-        print("Fetching " + key + " - " + str(remove_me) + "/" + str(len(keys)) + " Done.")
         current_dataset = get_data(key)
         current_dataset = current_dataset.rename(columns={'valueScaled': key})
-        #-------
-        #ONLY FOR TESTING!!!
+        # -------
+        # ONLY FOR TESTING!!!
         current_dataset['time'] = pd.to_datetime(current_dataset['time'])
         current_dataset = current_dataset.set_index('time')
         current_dataset = current_dataset.resample(rule='1S').bfill()
-        #-------
-        print(current_dataset)
+        # -------
         df = pd.merge(df, current_dataset, on='time', how='inner')
-        print(df)
-        remove_me += 1
-
-    print("Done fetching data.")
-    print(df["evaporator"])
     return df
+
+
+def model_to_dict(score, model, dependent_data_keys):
+    return {
+        "dependent": dependent_data_keys,
+        "score": float(score),
+        "model": model
+    }
 
 
 def train_model(all_data, prediction_unit):
@@ -76,22 +70,15 @@ def train_model(all_data, prediction_unit):
         test_size=test_sample_size,
         random_state=0)
     model.fit(independent_train, dependent_train)
-    score = float(model.score(independent_test, dependent_test))
 
-    model = {
-     "dependent": [dependent_data_keys],
-     "score": score,
-     "model": model
-    }
-    return model
+    return model_to_dict(model.score(independent_test, dependent_test), model, dependent_data_keys)
 
 
 def calculate_average_score(all_models):
     score_sum = 0
     for model in all_models:
         score_sum += model["score"]
-    print("Average score: " + str(score_sum/len(all_models)))
-    return float(score_sum/len(all_models))
+    return float(score_sum / len(all_models))
 
 
 def save_prediction_model(all_models, config):
@@ -102,20 +89,17 @@ def save_prediction_model(all_models, config):
         "models": all_models
     }
     model_persistor.save(persist_dictionary)
-    print("Model saved with average score: " + str(avg_score))
 
 
 def train(config):
-    print("Training models....")
     all_models = []
     all_data = get_all_data()
-    remove_me = 0
     for prediction_unit in config:
-        print("Creating model for " + prediction_unit["dependent"][0] + " - " + str(remove_me) + "/" + str(len(config)) + " Done.")
         all_models.append(train_model(all_data, prediction_unit))
     save_prediction_model(all_models, config)
 
 
+# ------------------------------ TESTONLY
 default = [
     {
         "independent": ["freshAirIntake"],
@@ -143,6 +127,5 @@ default = [
         "test_sample_size": 0.2
     }
 ]
-
 
 train(default)
