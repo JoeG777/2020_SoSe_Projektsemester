f552906ea36cca78bb82ba3c9dd12f49b3057cb9
Julian <juliang98@web.de>
2020-05-06 15:05:37 +0200

    Remove boilerplate code and added actual code

--- data_pipeline/vorhersage_berechnen/src/prediction_core/prediction_api/prediction_api.py
+++ data_pipeline/vorhersage_berechnen/src/prediction_core/prediction_api/prediction_api.py
@@ -1,7 +1,18 @@
-from flask import Flask
+from flask import *
+import data_pipeline.vorhersage_berechnen.src.prediction_core.training_engine.training_engine as training_engine
+import data_pipeline.vorhersage_berechnen.src.prediction_core.prediction_engine.prediction_engine as prediction_engine
+from data_pipeline.exception.exceptions import *
+
 
 app = Flask(__name__)
 
+http_status_codes = {
+    "HTTPBadRequest": 400,
+    "HTTPInternalServerError": 500,
+    "ConfigException": 900,
+    "DBException": 901,
+    "PersistorException": 902
+}
 
 @app.route('/')
 def default():
@@ -12,22 +23,54 @@ def default():
     return '<h1>Endpoints</h1><p>/train - Train a model</p><p>/predict - Make a prediction</p><p>/log - Get the logs</p>'
 
 
-@app.route('/train')
+@app.route('/train', methods=['POST'])
 def train():
     """
     Name in documentation: 'trainieren'
 
     """
-    return 'train'
+    status_code = 200
+    if request.is_json:
+        try:
+            #training_engine.train(request.get_json())
+            # TODO CALL FUNCTION ABOVE AND REMOVE RAISE BELOW
+            raise PersistorException
+        except InvalidConfigException: # TODO replace with generic config exception
+            status_code = http_status_codes.get("ConfigException")
+        except IndexError: # TODO replace with DBException
+            status_code = http_status_codes.get("DBException")
+        except PersistorException:
+            status_code = http_status_codes.get("PersistorException")
+        except Exception:
+            status_code = http_status_codes.get("HTTPInternalServerError")
+    else:
+        status_code = http_status_codes.get("HTTPBadRequest")
 
+    return Response(status=status_code)
 
-@app.route('/predict')
+@app.route('/predict', methods=['POST'])
 def predict():
     """
     Name in documentation: 'vorhersagen'
 
     """
-    return 'predict'
+    if request.is_json:
+        try:
+            #prediction_engine.predict(request.get_json())
+            # TODO CALL FUNCTION ABOVE AND REMOVE RAISE BELOW
+            raise PersistorException
+        except InvalidConfigException: # TODO replace with generic config exception
+            status_code = http_status_codes.get("ConfigException")
+        except IndexError: # TODO replace with DBException
+            status_code = http_status_codes.get("DBException")
+        except PersistorException:
+            status_code = http_status_codes.get("PersistorException")
+        except Exception:
+            status_code = http_status_codes.get("HTTPInternalServerError")
+    else:
+        status_code = http_status_codes.get("HTTPBadRequest")
+
+    return Response(status=status_code)
 
 
 @app.route('/log')
