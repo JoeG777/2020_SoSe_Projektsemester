09bb59e6a1da6b2a8f838d3542707a703b6be3fd
Matteo <1824001@stud.hs-mannheim.de>
2020-05-06 13:36:17 +0200

    'get_converted_date' changed so it sets the timezone to UTC.

--- data_pipeline/daten_erheben/src/utils/utils.py
+++ data_pipeline/daten_erheben/src/utils/utils.py
@@ -1,22 +1,25 @@
-from datetime import datetime, timedelta
+import datetime
+import pytz
 import data_pipeline.db_connector.src.write_manager.write_manager as wm
 
 
 def get_converted_date(date):
     '''
-    This method removes 2 hours from the passed date in order to show it correctly in Grafana.
+    This method sets the timezone of the timestamp to UTC in order to show it correctly in Grafana.
     :param date: Describes the Date you want to convert.
     :return: the date 2 hours before the passed date.
     '''
-    last_time = datetime(year = int(date[:4]), month = int(date[5:7]), day = int(date[8:10]), hour = int(date[11:13]), minute = int(date[14:16]), second = int(date[17:19]))
-    two_hours = timedelta(hours = 2)
 
-    time = str(last_time - two_hours)
-    time_split = time.split(" ")
+    timestamp = datetime(year = int(date[:4]), month = int(date[5:7]), day = int(date[8:10]), hour = int(date[11:13]), minute = int(date[14:16]), second = int(date[17:19]))
+    local_timezone = pytz.timezone("Europe/Berlin")
+    timezone = pytz.timezone("UTC")
 
-    return_time = time_split[0] + "T" + time_split[1] + "Z"
+    local_timestemp = local_timezone.localize(timestamp)
+    timezone_timestamp = local_timestemp.astimezone(timezone)
 
-    return return_time
+    timestamp_formatted = str(timezone_timestamp)[:10] + "T" + str(timezone_timestamp)[11:19] + "Z"
+
+    return timestamp_formatted
 
 
 def write_to_influx(json_array):
