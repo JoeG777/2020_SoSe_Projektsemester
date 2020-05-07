9ca11cfa25ef59b21519c0c0e6feeba6eefde278
Marius1822640 <mbrill963@gmail.com>
2020-05-07 15:02:22 +0200

    reworked exceptions

--- data_pipeline/exception/exceptions.py
+++ data_pipeline/exception/exceptions.py
@@ -1,10 +1,20 @@
-class RedundantConfigException(Exception):
+class DataPipelineException(Exception):
     def __init__(self, *args):
         if args:
             self.message = args[0]
         else:
             self.message = None
 
+
+class ConfigException(DataPipelineException):
+    def __str__(self):
+        if self.message:
+            return 'ConfigException: {0}'.format(self.message)
+        else:
+            return 'ConfigException has been raised'
+
+
+class RedundantConfigException(ConfigException):
     def __str__(self):
         if self.message:
             return 'RedundantConfigException: {0}'.format(self.message)
@@ -12,13 +22,7 @@ class RedundantConfigException(Exception):
             return 'RedundantConfigException has been raised'
 
 
-class IncompleteConfigException(Exception):
-    def __init__(self, *args):
-        if args:
-            self.message = args[0]
-        else:
-            self.message = None
-
+class IncompleteConfigException(ConfigException):
     def __str__(self):
         if self.message:
             return 'IncompleteConfigException: {0}'.format(self.message)
@@ -26,13 +30,7 @@ class IncompleteConfigException(Exception):
             return 'IncompleteConfigException has been raised'
 
 
-class InconsistentConfigException(Exception):
-    def __init__(self, *args):
-        if args:
-            self.message = args[0]
-        else:
-            self.message = None
-
+class InconsistentConfigException(ConfigException):
     def __str__(self):
         if self.message:
             return 'InconsistentConfigException: {0}'.format(self.message)
@@ -40,13 +38,7 @@ class InconsistentConfigException(Exception):
             return 'InconsistentConfigException has been raised'
 
 
-class InvalidTrainingPercentageException(Exception):
-    def __init__(self, *args):
-        if args:
-            self.message = args[0]
-        else:
-            self.message = None
-
+class InvalidTrainingPercentageException(ConfigException):
     def __str__(self):
         if self.message:
             return 'InvalidTrainingPercentageException: {0}'.format(self.message)
@@ -54,29 +46,41 @@ class InvalidTrainingPercentageException(Exception):
             return 'InvalidTrainingPercentageException has been raised'
 
 
-class InvalidConfigException(Exception):
-    def __init__(self, *args):
-        if args:
-            self.message = args[0]
-        else:
-            self.message = None
-
+class InvalidConfigKeyException(ConfigException):
     def __str__(self):
         if self.message:
-            return 'InvalidConfigException: {0}'.format(self.message)
+            return 'InvalidConfigKeyException: {0}'.format(self.message)
         else:
-            return 'InvalidConfigException has been raised'#
+            return 'InvalidConfigException has been raised'
 
 
-class PersistorException(Exception):
-    def __init__(self, *args):
-        if args:
-            self.message = args[0]
+class InvalidConfigValueException(ConfigException):
+    def __str__(self):
+        if self.message:
+            return 'InvalidConfigValueException: {0}'.format(self.message)
         else:
-            self.message = None
+            return 'InvalidConfigException has been raised'
 
+
+class AmbiguousConfigException(ConfigException):
+    def __str__(self):
+        if self.message:
+            return 'AmbiguousConfigException: {0}'.format(self.message)
+        else:
+            return 'AmbiguousConfigException has been raised'
+
+
+class PersistorException(DataPipelineException):
     def __str__(self):
         if self.message:
             return 'PersistorException: {0}'.format(self.message)
         else:
             return 'PersistorException has been raised'
+
+
+class DBException(DataPipelineException):
+    def __str__(self):
+        if self.message:
+            return 'DBException: {0}'.format(self.message)
+        else:
+            return 'DBException has been raised'
