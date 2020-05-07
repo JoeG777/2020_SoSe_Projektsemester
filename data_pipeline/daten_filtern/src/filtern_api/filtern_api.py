54eba9c888bfa66be987826ad17d28a3e0c95c51
Magnus Verclas <1727129@stud.hs-mannheim.de>
2020-05-07 11:06:26 +0200

    Api exeptions hinzugefügt

--- data_pipeline/daten_filtern/src/filtern_api/filtern_api.py
+++ data_pipeline/daten_filtern/src/filtern_api/filtern_api.py
@@ -1,18 +1,10 @@
 from flask import *
 from data_pipeline.daten_filtern.src.filtern_engine import filtern_engine
 from influxdb import InfluxDBClient
+from data_pipeline.daten_filtern.src.filtern_config.filtern_config import filtern_config
+from data_pipeline.exception.exceptions import ConfigException
 
 app = Flask(__name__)
-
-@app.route('/')
-def default():
-    '''
-
-    '''
-    return '<h1>Endpoints</h1><p>/filtern - filter data</p><p>/log - Get the logs</p>'
-
-
-
 
 @app.route('/filtern')
 def filter():
@@ -21,9 +13,20 @@ def filter():
     Start the filtern engine.
     '''
 
-    filtern_engine.filtern()
+    response = None
+    try:
 
-    return render_template('index.html')
+        config = filtern_config["filter_options"][filtern_config["selected_value"]]
+
+        filtern_engine.filtern(config)
+
+        response = 200
+
+    except ConfigException:
+        response = ConfigException.args[1]
+    finally:
+        return Response(status=response)
+
 
 
 @app.route('/log')
@@ -32,3 +35,6 @@ def get_logs():
     Name in documentation: 'get_logs'
     '''
     return 'logs'
+
+if __name__ == '__main__':
+    app.run(host='localhost', port='8000')
--- data_pipeline/daten_filtern/src/filtern_engine/filtern_engine.py
+++ data_pipeline/daten_filtern/src/filtern_engine/filtern_engine.py
@@ -4,20 +4,18 @@ from data_pipeline.exception.exceptions import DBException, ConfigException
 #import data_pipeline.log_writer.log_writer as log_writer
 import data_pipeline.db_connector.src.read_manager.read_manager as reader
 import data_pipeline.db_connector.src.write_manager.write_manager as writer
-from data_pipeline.daten_filtern.src.filtern_config.filtern_config import filtern_config
+
 
 #logger = log_writer.LogWriter()
 
 
-def filter():
+def filter(config):
     """
     Name in documentation: 'filtern'
     Takes the config and load the klassified data. After that it delete and interpolate the marked intervall. Last it persist the filtered data.
     :raises ConfigExeption: If the config is wrong.
     :return: A http status code.
     """
-
-    config = filtern_config["filter_options"][filtern_config["selected_value"]]
 
     filtern_data = get_data()
 
@@ -31,7 +29,7 @@ def filter():
 
     except:
         #logger.influx_logger.error("Config is wrong.")
-        raise ConfigException("Config is wrong.", 900)
+        raise ConfigException("Filtern Config is wrong.", 900)
 
     persist_data(filtern_data)
     pass
