99c3daaba846fb81733c0b52cf8cc242518ba616
Marius1822640 <mbrill963@gmail.com>
2020-05-08 11:30:44 +0200

    Added Tests

--- data_pipeline/db_connector/tests/write_manager/test_read_manager/test_read_manager.py
+++ data_pipeline/db_connector/tests/write_manager/test_read_manager/test_read_manager.py
@@ -1,7 +1,9 @@
 import unittest
-from mockito import mockito
-from mockito.mockito import when
+from mockito import mockito, ANY, mock
+from mockito.matchers import Any
+from mockito.mockito import when2, verify
 from data_pipeline.db_connector.src.read_manager import read_manager as rm
+from data_pipeline.db_connector.src.read_manager.read_manager import format_data as fd
 from influxdb import InfluxDBClient
 from influxdb import DataFrameClient
 
@@ -9,12 +11,16 @@ from influxdb import DataFrameClient
 class test_build_write_json(unittest.TestCase):
 
     def test_adds_all_values(self):
-        mock_client = mockito.Mock(InfluxDBClient)
-        when(mock_client).query(any()).thenReturn("test")
+        mock_client = mock(InfluxDBClient, {
+            "_host": "localhost"
+        })
+        when2(InfluxDBClient.__init__(mock_client, "localhost", 8086, "admin", "admin")).thenReturn(mock_client)
+        when2(mock_client.query("test_measurement")).thenReturn("test")
+        when2(rm.format_data, "test").thenReturn("success!")
 
-        output = rm.build_write_json("test_measurement", "1", "test_value", "1")
+        output = rm.read_query("test", "test_measurement")
 
-        self.assertEqual(expected_json, output)
+        self.assertEqual(output, "success!")
 
 
 if __name__ == '__main__':
