c385b9b8fb4e413f56cfb3714e2c8eb2fe01792c
NikolasSchwab99 <1821623@stud.hs-mannheim.de>
2020-05-11 15:29:29 +0200

    API designed including PyDoc

--- data_pipeline/konfiguration/bereinigungs_config.py
+++ data_pipeline/konfiguration/bereinigungs_config.py
@@ -1,7 +1,7 @@
 bereinigungs_config = {
     'frame_width': 100,
     'freq': '60S',
-    'register':  ['inlet', 'outlet'],
+    'register':  'inlet, outlet',
 
     'time': {
         'from': '1579561622474ms',
--- data_pipeline/konfiguration/config_API.py
+++ data_pipeline/konfiguration/config_API.py
@@ -1,128 +1,79 @@
 from flask import *
-import data_pipeline.exception.exceptions as exceptions
-from data_pipeline.log_writer.log_writer import Logger
+from data_pipeline.konfiguration.bereinigungs_config import bereinigungs_config as be_con
+from data_pipeline.konfiguration.erhebungs_config import erhebungs_config as er_con
+from data_pipeline.konfiguration.filterungs_config import filtern_config as fi_con
+from data_pipeline.konfiguration.klassifikations_config import classification_config as kl_con
+from data_pipeline.konfiguration.vorhersage_config import vorhersage_config as vs_con
+from data_pipeline.konfiguration.frontend_interface_config import frontend_interface_config as fei_con
+
 app = Flask(__name__)
-
-logger = Logger()
-
-
-def check_request(req_json):
-    """
-    Given the valid schema of a http-Post-requests, this function extracts all keys from the users request
-    'req_json' and compares them to the given schema-keys. Only if both lists contain the same
-    keys, true will be returned.
-    :param req_json: post-request-object.
-    :raise InvalidConfigException if the given object is null.
-    :return True if the ecepted keys match the given keys in the request.
-    """
-
-    expected_keys = set(['frame_width', 'freq', 'register', 'time', 'from', 'to', 'threshold'])
-
-    def get_keys(json_req):
-
-        tmp = []
-
-        def get_keys_rec(v_dict):
-            for k, v in v_dict.items():
-                if type(k) is dict:
-                    tmp.append(k)
-                    get_keys_rec(v)
-                else:
-                    tmp.append(k)
-
-        for k, v in json_req.items():
-            if type(v) is dict:
-                tmp.append(k)
-                get_keys_rec(v)
-            else:
-                tmp.append(k)
-
-        return tmp
-
-    if req_json is None:
-        raise exceptions.InvalidConfigException('Request empty', 900)
-
-    appendix = set(get_keys(req_json))
-
-    return appendix == expected_keys
-
-
-def check_if_request_empty():
-    """ Checks if the incoming http-post-request comes with an empty body.
-    :raise InvalidConfigException if the body is empty.
-    :return request-object.
-    """
-    if int(request.headers.get('Content-Length')) == 0:
-        raise exceptions.InvalidConfigException('Message must not be empty', 900)
-    return request.get_json(force=True)
 
 
 @app.route('/erhebung_config', methods=['POST'])
 def erhebung_config():
     """
-    Name in documentation: datenbereinigung()
-    API callable via http-post-request when existing data needs to be cleaned. While executing every occurring
-    error/warning will be logged into the log-database specified in db_connector.
-    :return JSON-respond with status code 200 if call was successful and 900 otherwise.
-     """
+    Name in documentation: erhebung_config()
+    API callable via http-post-request when the pipeline-controller needs to access the config JSON-file for
+    service Daten erheben.
+    :return JSON-response with config.
+    """
+    return make_response(er_con)
 
-    response = {'statuscode': 200}
-
-    try:
-
-        pass
-
-    except exceptions.InvalidConfigException as ice:
-        response['statuscode'] = 900
-        logger.influx_logger.error(ice.args[0])
-
-    except exceptions.InconsistentConfigException as ice:
-        response['statuscode'] = 900
-        logger.influx_logger.error(ice.args[0])
-
-    except exceptions.NoDataException as nde:
-        response['statuscode'] = 900
-        logger.influx_logger.error(nde.args[0])
-
-    except exceptions.FormatException as fe:
-        response['statuscode'] = 900
-        logger.influx_logger.error(fe.args[0])
-
-    except exceptions.ImputationDictionaryException as ide:
-        response['statuscode'] = 900
-        logger.influx_logger.error(ide.args[0])
-
-    except exceptions.DBError as dbe:
-        response['statuscode'] = 901
-        logger.influx_logger.error(dbe.args[0])
-
-    except Exception as e:
-        response['statuscode'] = 500
-        logger.influx_logger.error(e.args[0])
-
-    finally:
-        logger.influx_logger.info('bereinigungsprozess beendet mit statuscode' + str(response['statuscode']))
-        return make_response(jsonify({'statuscode': response['statuscode']}))
 
 @app.route('/bereinigung_config', methods=['POST'])
 def bereinigung_config():
-    pass
+    """
+   Name in documentation: bereinigung_config()
+   API callable via http-post-request when the pipeline-controller needs to access the config JSON-file for
+   service Daten bereinigen.
+   :return JSON-response with config.
+   """
+    return make_response(be_con)
+
 
 @app.route('/filterung_config', methods=['POST'])
 def filterung_config():
-    pass
+    """
+   Name in documentation: filterung_config()
+   API callable via http-post-request when the pipeline-controller needs to access the config JSON-file for
+   service Daten filtern.
+   :return JSON-response with config.
+   """
+    return make_response(fi_con)
+
 
 @app.route('/klassifikation_config', methods=['POST'])
 def klassifikation_config():
-    pass
+    """
+   Name in documentation: klassifikation_config()
+   API callable via http-post-request when the pipeline-controller needs to access the config JSON-file for
+   service Daten klassifizieren.
+   :return JSON-response with config.
+   """
+    return make_response(kl_con)
+
 
 @app.route('/vorhersage_config', methods=['POST'])
 def vorhersage_config():
-    pass
+    """
+   Name in documentation: vorhersage_config()
+   API callable via http-post-request when the pipeline-controller needs to access the config JSON-file for
+   service Vorhersage berechnen.
+   :return JSON-response with config.
+   """
+    return make_response(vs_con)
+
 
 @app.route('/frontend_interface_config', methods=['POST'])
 def frontend_interface_config():
-    pass
+    """
+   Name in documentation: frontend_interface_config()
+   API callable via http-post-request when the pipeline-controller needs to access the config JSON-file for
+   service Frontend-Interface.
+   :return JSON-response with config.
+   """
+    return make_response(fei_con)
+
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=5000)
