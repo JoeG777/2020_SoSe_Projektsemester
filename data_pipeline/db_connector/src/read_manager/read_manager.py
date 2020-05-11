328448421d5a4d333efbfc136dd864b1e8bf2ecf
ArbnorZ <45542677+ArbnorZ@users.noreply.github.com>
2020-05-11 11:30:15 +0200

    Rebased stuff we need

--- data_pipeline/db_connector/src/read_manager/read_manager.py
+++ data_pipeline/db_connector/src/read_manager/read_manager.py
@@ -3,57 +3,65 @@ import pandas as pd
 from influxdb import InfluxDBClient
 from ..db_config import db_config
 
+default_measurement = "temperature"
+default_register = "201"
+
 url = db_config.params.get("url")
 port = db_config.params.get("port")
 user = db_config.params.get("user")
 password = db_config.params.get("password")
-db = db_config.params.get("db_name")
+
+register_dict = {
+    "freshAirIntake": "201",
+    "inlet": "202",
+    "room": "210",
+    "outlet": "204",
+    "condenser": "205",
+    "evaporator": "206"
+}
 
 
-def read_query(query):
+def read_query(db, query):
     """
     Takes a query in the influx sql dialect. Sends this query to the client provided by the current config and returns
     the queries result. Calls format_data() on all retrieved sets.
+    :param db: The database the query should be read from.
     :param query: The query for the data retrieval.
     :return: The result of the query.
     """
     client = InfluxDBClient(url, port, user, password, db)
     data = client.query(query)
-    points = list(data.get_points())
-    result = []
-
-    for p in points:
-        result.append(p.get('valueScaled'))
-
-    return format_data(result)
+    return format_data(data.get_points())
 
 
-def read_register_of_measurement(measurement, register):
+def read_data(db, **kwargs):
     """
-    Takes a measurement and a register, both as Strings, and returns the value present in these.
-    :param measurement: The measurement the data should be retrieved from.
-    :param register: The register the data should be retrieved from.
+    Takes a String that resembles the database the influx client should connect to. Further takes these
+    optional arguments (note that ALL arguments need to be provided as Strings):
+    measurement: define a specific measurement the data should be retrived from.
+    register: The number of the register the data should be read from.
+    resolve_register: If this variable is set, you can just use the curves names as defined in the register_dict.
+                      The register names are mapped to the respective numbers.
+    start_utc: The start date and time formatted in utc.
+    end_utc: The start date and time formatted in utc.
+    :param db: The database the data should be read from.
     :return: The retrieved data.
     """
+    measurement = default_measurement
+    if "measurement" in kwargs.keys():
+        measurement = kwargs["measurement"]
+    register = default_register
+    if "register" in kwargs.keys():
+        if "resolve_register" in kwargs.keys():
+            register = register_dict[register]
+        else:
+            register = kwargs["register"]
     query = 'select * from ' + measurement + ' where register = \'' + register + '\''
-    return read_query(query)
-
-
-def read_register_of_measurement_from_to(measurement, register, start, end):
-    """
-    Equal functionality as read_register_of_measurement, but does also take a start and an end time.
-    Make sure to format time as "yyyy-mm-dd hh:mm:ss".
-    :param measurement: The measurement the data should be retrieved from.
-    :param register: The register the data should be retrieved from.
-    :param start: The start date of the data set.
-    :param end: The end date of the data set.
-    :return: The retrieved data.
-    """
-    query = 'select * from ' + measurement \
-            + 'where time > \'' + start \
-            + '\' and time < \'' + end \
-            + '\' and register = \'' + register + '\''
-    return read_query(query)
+    if "start_utc" in kwargs.keys():
+        query += 'AND time >' + kwargs["start"]
+    if "end_utc" in kwargs.keys():
+        query += 'AND time <' + kwargs["end"]
+    return read_query(db, query)
 
 
 def format_data(dataset):
@@ -62,4 +70,7 @@ def format_data(dataset):
     :param dataset: The dataset to format.
     :return: The given dataset as a DataFrame.
     """
-    return pd.DataFrame(dataset)
+    df = pd.DataFrame(dataset)
+    df['time'] = pd.to_datetime(df['time'])
+    df = df.set_index('time')
+    return df
---
+++ data_pipeline/exception/exceptions.py
@@ -0,0 +1,92 @@
+class DataPipelineException(Exception):
+    def __init__(self, *args):
+        if args:
+            self.message = args[0]
+        else:
+            self.message = None
+
+    def __str__(self):
+        if self.message:
+            return 'RedundantConfigException: {0}'.format(self.message)
+        else:
+            return 'RedundantConfigException has been raised'
+
+
+class ConfigException(DataPipelineException):
+    def __str__(self):
+        if self.message:
+            return 'ConfigException: {0}'.format(self.message)
+        else:
+            return 'ConfigException has been raised'
+
+
+class IncompleteConfigException(ConfigException):
+    def __str__(self):
+        if self.message:
+            return 'IncompleteConfigException: {0}'.format(self.message)
+        else:
+            return 'IncompleteConfigException has been raised'
+
+
+class InconsistentConfigException(ConfigException):
+    def __str__(self):
+        if self.message:
+            return 'InconsistentConfigException: {0}'.format(self.message)
+        else:
+            return 'InconsistentConfigException has been raised'
+
+
+class InvalidTrainingPercentageException(ConfigException):
+    def __str__(self):
+        if self.message:
+            return 'InvalidTrainingPercentageException: {0}'.format(self.message)
+        else:
+            return 'InvalidTrainingPercentageException has been raised'
+
+
+class InvalidConfigKeyException(ConfigException):
+    def __str__(self):
+        if self.message:
+            return 'InvalidConfigKeyException: {0}'.format(self.message)
+        else:
+            return 'InvalidConfigException has been raised'
+
+
+class InvalidConfigValueException(ConfigException):
+    def __str__(self):
+        if self.message:
+            return 'InvalidConfigValueException: {0}'.format(self.message)
+        else:
+            return 'InvalidConfigException has been raised'
+
+
+class AmbiguousConfigException(ConfigException):
+    def __str__(self):
+        if self.message:
+            return 'AmbiguousConfigException: {0}'.format(self.message)
+        else:
+            return 'AmbiguousConfigException has been raised'
+
+
+class ConfigTypeException(ConfigException):
+    def __str__(self):
+        if self.message:
+            return 'ConfigTypeException: {0}'.format(self.message)
+        else:
+            return 'ConfigTypeException has been raised'
+
+
+class PersistorException(DataPipelineException):
+    def __str__(self):
+        if self.message:
+            return 'PersistorException: {0}'.format(self.message)
+        else:
+            return 'PersistorException has been raised'
+
+
+class DBException(DataPipelineException):
+    def __str__(self):
+        if self.message:
+            return 'DBException: {0}'.format(self.message)
+        else:
+            return 'DBException has been raised'
