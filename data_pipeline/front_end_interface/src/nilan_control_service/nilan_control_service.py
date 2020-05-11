6828546427b18425f2917ca3aeeb49c7b610bc1e
ArbnorZ <45542677+ArbnorZ@users.noreply.github.com>
2020-05-11 15:08:38 +0200

    added docs to nilan_control_service

--- data_pipeline/front_end_interface/src/nilan_control_service/nilan_control_service.py
+++ data_pipeline/front_end_interface/src/nilan_control_service/nilan_control_service.py
@@ -2,12 +2,12 @@ from datetime import datetime
 import data_pipeline.db_connector.src.write_manager.write_manager.py as wm
 import pytz
 
-def get_current_time_utc():
 
+def get_current_time_utc():
     '''
-    Name in documentation: ''
-    Writes the User-Settings into
-    :raises
+    Name in documentation: 'get_current_time'
+    This method sets the timezone of the timestamp to UTC in order to show it correctly in Grafana.
+    :return: the date 2 hours before the passed date.
     '''
 
     local_timezone = pytz.timezone("Europe/Berlin")
@@ -18,46 +18,42 @@ def get_current_time_utc():
 
     return str(timezone_timestamp)[:10] + "T" + str(timezone_timestamp)[11:19] + "Z"
 
-def format_json(json):
 
+def format_json(json):
     '''
     Name in documentation: 'formatiere_eingabedaten()'
-    Writes the User-Settings into
-    :param json: Download-link for the weather data from DWD
-    :raises
+    Formats the Json-File for InfluxDB
+    :param json: Nilan-Ventilation Unit Parameters
     '''
 
     json_nilan_array = [
         {'measurement': 'raumtemperatur',
          "time": get_current_time_utc(),
-         "fields":{"temperatur":json['raumtemperatur']}
+         "fields": {"temperatur": json['raumtemperatur']}
          },
         {'measurement': 'luefterstufe_zuluft',
          "time": get_current_time_utc(),
-         "fields":{"stufe":json['luefterstufe_zuluft']}
+         "fields": {"stufe": json['luefterstufe_zuluft']}
          },
         {'measurement': 'luefterstufe_abluft',
          "time": get_current_time_utc(),
-         "fields":{"stufe":json['luefterstufe_abluft']}
+         "fields": {"stufe": json['luefterstufe_abluft']}
          },
         {'measurement': 'betriebsmodus',
          "time": get_current_time_utc(),
-         "fields":{"modus":json['betriebsmodus']}
+         "fields": {"modus": json['betriebsmodus']}
          }
     ]
 
     return json_nilan_array
 
-def write_to_nilan(json):
 
+def write_to_nilan(json):
     '''
     Name in documentation: 'schreibe_eingabedaten()'
     Writes the User-Settings into Nilan-Ventilation Unit
-    :param json:
+    :param json: Nilan-Ventilation Unit Parameters
     :raises
     '''
 
     wm.write_to_influx_json("db_steuerungsparameter", format_json(json))
-
-
-
