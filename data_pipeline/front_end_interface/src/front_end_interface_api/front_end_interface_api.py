2ae2bf8713ed1299ff2d2a4076d579234ccddc23
Matteo <1824001@stud.hs-mannheim.de>
2020-05-11 11:36:11 +0200

    Started implementing front_end_interface_api.

--- data_pipeline/front_end_interface/src/front_end_interface_api/front_end_interface_api.py
+++ data_pipeline/front_end_interface/src/front_end_interface_api/front_end_interface_api.py
@@ -1,13 +1,28 @@
 import flask
 import front_end_interface.src.nilan_control_service as ncs
 import front_end_interface.src.pipeline_control_service as pcs
+import requests
 
 app = Flask(__name__)
 
 @app.route('/nilan_control_service')
 def nilan_control_service():
 
-    ncs.write_to_nilan()
+    respone = None
+
+    nilan_json = {
+        "startdatum": requests.get('start_datum'),
+        "enddatum": requests.get('end_datum'),
+        "vorhersage": requests.get('vorhersage'),
+        "raumtemperatur": requests.get('raumtemperatur'),
+        "luefterstufe_zuluft": requests.get('luefterstufe_zuluft'),
+        "luefterstufe_abluft": requests.get('luefterstufe_abluft'),
+        "betriebsmodus": requests.get('betriebsmodus')
+    }
+
+    ncs.write_to_nilan(nilan_json)
+
+    response = 200
 
 @app.route('/pipeline_control_service')
 def pipeline_control_service():
