5c40e03bd0aedf73d2b1b1c8be02c48d7a1d06a8
FelixEngelen <1822158@stud.hs-mannheim.de>
2020-05-11 13:44:21 +0200

    config test 4

--- data_pipeline/daten_filtern/tests/test_config.py
+++ data_pipeline/daten_filtern/tests/test_config.py
@@ -4,28 +4,21 @@ import unittest
 
 class Filtern_engine_tests(unittest.TestCase):
 
+    config = filtern_config["filter_options"][filtern_config["selected_value"]]
 
-
-    def test_all_curve(self):
+    def test_all_curve(self, config):
 
         #expected_data
         expected_data = (["room","condenser","evaporator","inlet","outlet","freshAirIntake"])
-
         print(expected_data)
 
         #real_data
-
-        config = filtern_config["filter_options"][filtern_config["selected_value"]]
-        #print(config)
-        #real_data = filtern_config["filter_options"][filtern_config["selected_value"][filtern_config[""]]]
         real_data = config.keys()
         print(real_data)
 
         self.assertCountEqual(expected_data, expected_data)
 
-    def test_cycle(self):
-
-        config = filtern_config["filter_options"][filtern_config["selected_value"]]
+    def test_all_cycle(self, config):
 
         #expected_data
         expected_data = (["WarmWasserZyklus","OfenZyklus","LüfterZyklus","AbtauZyklus"])
@@ -37,13 +30,10 @@ class Filtern_engine_tests(unittest.TestCase):
 
         self.assertCountEqual(expected_data, expected_data)
 
-
-    def text_delete(self):
-
-        config = filtern_config["filter_options"][filtern_config["selected_value"]]
+    def test_in_cycle(self, config):
 
         #expected_data
-        expected_data = (["delete", "Interpolation"])
+        expected_data = (["delete","Interpolation"])
         print(expected_data)
 
         #real_data
@@ -51,4 +41,7 @@ class Filtern_engine_tests(unittest.TestCase):
         print(real_data)
 
         self.assertCountEqual(expected_data, expected_data)
+
+
+
 
