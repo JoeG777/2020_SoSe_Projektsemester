a117c08f6537dd044d87b342edf1bd5661f3e099
Matteo <1824001@stud.hs-mannheim.de>
2020-05-12 11:33:35 +0200

    Started tests for nilan_control_service

--- data_pipeline/front_end_interface/tests/test_nilan_control_service.py
+++ data_pipeline/front_end_interface/tests/test_nilan_control_service.py
@@ -1,10 +1,18 @@
 import unittest
-import data_pipeline.front_end_interface.src.nilan_control_service.nilan_control_service as ncs
+import data_pipeline.front_end_interface.src.nilan_control_service as ncs
+from datetime import datetime, timedelta
 
 class MyTestCase(unittest.TestCase):
     def get_current_time_utc(self):
-        self.assertEqual(ncs.get_current_time_utc("2020-01-05T02:00:00Z"), "2020-01-05T00:00:00Z")
+        self.assertEqual(ncs.get_current_time_utc(), calculate_time())
 
+def calculate_time():
+
+    time_now = datetime.now()
+    time_difference = timedelta(hours=2)
+    time_new = time_now - time_difference
+
+    return time_new
 
 if __name__ == '__main__':
     unittest.main()
