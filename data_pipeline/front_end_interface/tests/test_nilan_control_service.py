95e88ded8681228b634b391245380182d8121de1
JensPreussler <jenspreussler@online.de>
2020-05-12 11:23:27 +0200

    test anfang

--- data_pipeline/front_end_interface/tests/test_nilan_control_service.py
+++ data_pipeline/front_end_interface/tests/test_nilan_control_service.py
@@ -1,9 +1,9 @@
 import unittest
-
+import data_pipeline.front_end_interface.src.nilan_control_service.nilan_control_service as ncs
 
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
+    def get_current_time_utc(self):
+        self.assertEqual(ncs.get_current_time_utc("2020-01-05T02:00:00Z"), "2020-01-05T00:00:00Z")
 
 
 if __name__ == '__main__':
