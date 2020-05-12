706925a3b5bdcfd2fb096026abda1e08d9742459
Matteo <1824001@stud.hs-mannheim.de>
2020-05-12 09:18:19 +0200

    Added docu on pipeline_control_service.py

--- data_pipeline/front_end_interface/src/pipeline_control_service/pipeline_control_service.py
+++ data_pipeline/front_end_interface/src/pipeline_control_service/pipeline_control_service.py
@@ -1,6 +1,11 @@
 import data_pipeline.exception.exceptions as exc
 
 def start_process(parameters):
+    '''
+    Calls the 'start_process'-method in the pipeline controller, which starts the whole forecast process.
+    :param parameters:
+    :return:
+    '''
 
     try:
         pipeline_controller.start_process(parameters)
