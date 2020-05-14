ae05b3498e58e987add7d6d81a0e5ef0cbebdd81
Marius1822640 <mbrill963@gmail.com>
2020-05-14 09:17:44 +0200

    Added logger.

--- data_pipeline/log_writer/log_writer.py
+++ data_pipeline/log_writer/log_writer.py
@@ -3,13 +3,24 @@ import logging
 
 class Logger():
 
-    def __init__(self):
-
-        self.influx_handler = InfluxHandler(database="logs", measurement="logs")
+    def __init__(self, component, database='logs', measurement='logs', host='localhost', port=8086):
+        self.COMPONENT = component
+        self.influx_handler = InfluxHandler(database=database,
+                                            measurement=measurement,
+                                            host=host, port=port,
+                                            extra_fields="component"
+                                            )
         logging.getLogger().setLevel(logging.DEBUG)
-
-        self.influx_logger = logging.getLogger('influx_logging.tests.simple_message')
+        self.influx_logger = logging.getLogger('influx_logging')
         self.influx_logger.addHandler(self.influx_handler)
+
+    def write_into_measurement(self, measurement, content):
+        default_measurement = self.influx_handler.measurement
+        self.influx_handler.measurement = measurement
+        self.influx_logger.info(content, extra={"component": self.COMPONENT})
+        self.influx_handler.measurement = default_measurement
+
+
 
 
 
