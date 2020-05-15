80f1313353a371440372cea0ed761c033b32eb96
FelixEngelen <1822158@stud.hs-mannheim.de>
2020-05-15 09:29:43 +0200

    config validator new named exceptions

--- data_pipeline/daten_filtern/src/filtern_validator/filtern_validator.py
+++ data_pipeline/daten_filtern/src/filtern_validator/filtern_validator.py
@@ -15,7 +15,7 @@ def config_validation(filtern_config):
         timeframe = filtern_config['timeframe']
     except:
         #logger.influx_logger.error("Config is wrong.")
-        raise exe.ConfigException("Can not read Filtern Config.", 900)
+        raise exe.InvalidConfigKeyException("Can not read Filtern Config.", 900)
 
 
     expected_curve = ['room', 'condenser', 'evaporator', 'inlet', 'outlet', 'freshAirIntake']
@@ -32,18 +32,18 @@ def config_validation(filtern_config):
                     expected_delete_interpolation.remove(delete_interpolation)
 
             if expected_delete_interpolation != []:
-                raise exe.ConfigException("Filtern Config got no Interpolation or Delete.", 900)
+                raise exe.InvalidConfigKeyException("Filtern Config got no Interpolation or Delete.", 900)
 
             if cycle in expected_cycle:
                 expected_cycle.remove(cycle)
             if config[curve][cycle]["delete"] != 'True' and config[curve][cycle]["delete"] != 'False':
-                raise exe.ConfigException("Filtern Config Delete is not True or False.", 900)
+                raise exe.InvalidConfigValueException("Filtern Config Delete is not True or False.", 900)
             if config[curve][cycle]["Interpolation"] != 'linear' and config[curve][cycle]["Interpolation"] != 'cubic' and config[curve][cycle]["Interpolation"] != 'spline' and config[curve][cycle]["Interpolation"] != 'akima':
-                raise exe.ConfigException("Filtern Config Interpolation is not linear, cubic, spline or akima.", 900)
+                raise exe.InvalidConfigValueException("Filtern Config Interpolation is not linear, cubic, spline or akima.", 900)
 
         if expected_cycle != []:
-            raise exe.ConfigException("Filtern Config missing a cycle.", 900)
+            raise exe.IncompleteConfigException("Filtern Config missing a cycle.", 900)
 
     if expected_curve != []:
-        raise exe.ConfigException("Filtern Config missing a curve.", 900)
+        raise exe.IncompleteConfigException("Filtern Config missing a curve.", 900)
 
