e5f15dd41e9c0b49a0ffc0c1e3d044d362301f9b
ArbnorZ <45542677+ArbnorZ@users.noreply.github.com>
2020-05-18 09:51:57 +0200

    added front_end_json as params for start process in pipelinecontroller

--- data_pipeline/front_end_interface/src/pipeline_control_service/pipeline_control_service.py
+++ data_pipeline/front_end_interface/src/pipeline_control_service/pipeline_control_service.py
@@ -1,17 +1,19 @@
+import json
 import data_pipeline.exception.exceptions as exc
 import data_pipeline.log_writer.log_writer as log_writer
+import requests
 
 logger = log_writer.Logger()
 
-def start_process(parameters):
+
+def build_request_data_pipeline_cmd(front_end_json):
     '''
     Calls the 'start_process'-method in the pipeline controller, which starts the whole forecast process.
     :param parameters: Parameters from Usersettings in the Web-application
     '''
 
     try:
-        pipeline_controller.start_process(parameters)
-
+        return requests.post("http://localhost:5000" + "/start_process ", json=json.dumps(json))
     except:
         logger.influx_logger.error('Unable to start process')
         raise exc.RawDataException('Unable to start process', 905)
